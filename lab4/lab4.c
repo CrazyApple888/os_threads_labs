5cfa04b489b099cd80dc490d80910de596161b02
a.isachenko <a.isachenko@g.nsu.ru>
2021-09-02 22:42:21 +0700

    Updated lab4

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -15,14 +15,17 @@ void *do_job(void *_) {
 int main() {
     pthread_t child;
     if (pthread_create(&child, NULL, do_job, NULL) != 0) {
-        perror("Can't create thread\n");
+        printf("Can't create thread\n");
         return 1;
     }
     sleep(SLEEP_TIME);
     if (pthread_cancel(child) != 0) {
-        perror("Can't cancel thread :(\n");
+        printf("Can't cancel thread :(\n");
         return 1;
     } else {
+        if (pthread_join(child, NULL) != 0) {
+            printf("Can't join thread\n");
+        }
         printf("Thread canceled :)\n");
     }
     return 0;
